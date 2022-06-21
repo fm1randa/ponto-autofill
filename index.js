fd195a74ceb072c70e85b4c8e935bd6eca7fea2f
Filipe Miranda <filipe.rj149@gmail.com>
2022-06-21 18:47:28 -0300

    feat: add functionality to autofill hours
    also add some refactor

--- index.js
+++ index.js
@@ -35,7 +35,6 @@
 
     window.document.getElementsByTagName("html")[0].focus();
 
-    let titleFound = false;
     let interval = setInterval(() => {
         const { titleDocument, markDocument } = fetchDocuments();
         const getInputs = (inputNames) => {
@@ -43,15 +42,34 @@
                 return markDocument.getElementsByName(name)[0];
             });
         };
-        if (titleDocument) {
-            titleFound = true;
-
+        const getDateValue = () => {
             const headerDateInput =
                 titleDocument.getElementsByName("lbDesData")[0];
             const headerDateValue = headerDateInput.value;
-
-            const parsedDateValue = headerDateValue.split("-")[0].trim();
+            return headerDateValue.split("-")[0].trim();
+        };
+        const getHourValue = () => {
+            const headerHourInput =
+                titleDocument.getElementsByName("lbDesHor")[0];
+            const headerHourValue = headerHourInput.value;
+            return headerHourValue.split(" ").slice(2, 6);
+        };
+        const fillInputs = (inputs, value) => {
+            inputs.forEach((input) => {
+                input.value = value;
+            });
+        };
+        const fillHour = (inputs, value) => {
+            inputs.forEach((input, index) => {
+                input.value = value[index];
+            });
+        };
+        if (titleDocument) {
+            const date = getDateValue();
+            const hour = getHourValue();
+            const justificationCode = "2";
             const dateInputNames = ["DDM01", "DDM02", "DDM03", "DDM04"];
+            const hourInputNames = ["DHM01", "DHM02", "DHM03", "DHM04"];
             const justificationCodeInputNames = [
                 "DJU01",
                 "DJU02",
@@ -59,18 +77,16 @@
                 "DJU04",
             ];
             const dateInputs = getInputs(dateInputNames);
+            const hourInputs = getInputs(hourInputNames);
             const justificationCodeInputs = getInputs(
                 justificationCodeInputNames
             );
             if (!dateInputs[0])
                 return console.warn("PontoAutofill failed to find inputs");
 
-            dateInputs.forEach((input) => {
-                input.value = parsedDateValue;
-            });
-            justificationCodeInputs.forEach((input) => {
-                input.value = "2";
-            });
+            fillInputs(dateInputs, date);
+            fillHour(hourInputs, hour);
+            fillInputs(justificationCodeInputs, justificationCode);
             console.log("PontoAutofill executed sucessfully :)");
             return clearInterval(interval);
         }
